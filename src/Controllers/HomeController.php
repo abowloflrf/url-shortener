dcf53b4ea10e87f98c75a7184f97a2b0174af69a
abowloflrf <abowloflrf@outlook.com>
2017-11-04 16:55:11 +0800

    Complete basic function

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -16,7 +16,7 @@ class HomeController
     public function home($request, $response, $args)
     {
         $pdo = $this->container->get('db');
-        $stmt = $pdo->prepare("SELECT * FROM url");
+        $stmt = $pdo->prepare("SELECT * FROM url LIMIT 10");
         $stmt->execute();
         $result = $stmt->fetchAll();
         $this->container->get('renderer')->render($response, 'home.phtml', [
@@ -27,23 +27,36 @@ class HomeController
 
     public function create($request, $response, $args)
     {
+        //TODO: 判断url是否已经在数据库存在若存在直接返回短链接
         $body = $request->getParsedBody();
-        $fullurl=$body['fullurl'];
+        //trim去除首位空白字符
+        $fullurl = trim($body['fullurl'], " \t\n\r\0\x0B/") . '/';
+        //只允许(http|ftp)s?协议，输入时需要添加
+        if (!empty($fullurl) && preg_match('/^(http|ftp)s?:\/\//', $fullurl)) {
+            //获取表中的auto_increment
+            $pdo = $this->container->get('db');
+            $stmt = $pdo->prepare("SELECT auto_increment FROM information_schema.`TABLES` WHERE TABLE_SCHEMA='url-shortener' AND TABLE_NAME='url'");
+            $stmt->execute();
+            $id = (int)$stmt->fetch()['auto_increment'];
+            //将auto_increment转为62进制作为短域名
+            $shortened_url = UrlTrans::IDtoURL($id);
+            $stmt_insert = $pdo->prepare("INSERT INTO url (url_short,url_full) VALUES (:url_short,:url_full)");
+            $stmt_insert->bindParam(':url_short', $shortened_url);
+            $stmt_insert->bindParam(':url_full', $fullurl);
+            $stmt_insert->execute();
+            return json_encode(array(
+                'status' => 'success',
+                'id' => $id,
+                'url_s' => $shortened_url,
+                'url_f' => $fullurl
+            ));
+        }
+        else {
+            return json_encode(array(
+                'status' => 'fail',
+                'msg' => 'Illegal url, please check your url again.'
+            ));
+        }
 
-        $pdo = $this->container->get('db');
-        $stmt = $pdo->prepare("SELECT auto_increment FROM information_schema.`TABLES` WHERE TABLE_SCHEMA='url-shortener' AND TABLE_NAME='url'");
-        $stmt->execute();
-        $id=(int)$stmt->fetch()['auto_increment'];
-        $shortened_url=UrlTrans::IDtoURL($id);
-        $stmt_insert = $pdo->prepare("INSERT INTO url (url_short,url_full) VALUES (:url_short,:url_full)");
-        $stmt_insert->bindParam(':url_short',$shortened_url);
-        $stmt_insert->bindParam(':url_full',$fullurl);
-        $stmt_insert->execute();
-        return json_encode(array(
-            'msg'=>'success',
-            'id'=>$id,
-            'url_s'=>$shortened_url,
-            'url_f'=>$fullurl
-        ));
     }
 }
--- src/Controllers/UrlController.php
+++ src/Controllers/UrlController.php
@@ -5,6 +5,8 @@ use Slim\Container;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use App\Utils\UrlTrans;
+
 class UrlController
 {
 
@@ -17,7 +19,25 @@ class UrlController
 
     public function index(Request $request, Response $response, array $args)
     {
-        //TODO:处理短域名解析并跳转
-        return $args['url'];
+        //短域名格式不匹配，返回404
+        if (!preg_match('/^[a-zA-Z0-9]{4,6}$/', $args['url'])) {
+            return $response->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('404 Page not found');
+        }
+        //从数据库中查询完整域名并跳转
+        $pdo = $this->container->get('db');
+        $stmt = $pdo->prepare("SELECT url_full FROM url WHERE url_short ='" . $args['url'] . "'");
+        //TODO: 统计点击量/refer
+        $stmt->execute();
+        $result = $stmt->fetch();
+        if ($result) {
+            return $response->withRedirect($result['url_full'], 301);
+        }
+        //未查询到域名则跳转到首页
+        else {
+            return $response->withRedirect('/', 301);
+        }
+
     }
 }
