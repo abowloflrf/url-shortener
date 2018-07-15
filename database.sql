29e26a1b5e204593d0951776959772969ab6720f
ruofeng <lrf0829@126.com>
2018-07-15 21:42:06 +0800

    fix sql

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE TABLE `url` (
 	`url_full` TEXT NULL COLLATE 'utf8_bin',
 	`click` INT(10) UNSIGNED NOT NULL DEFAULT '0',
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`key`),
+	PRIMARY KEY (`id`),
 	UNIQUE INDEX `url_short` (`url_short`)
 )
 COLLATE='utf8_bin'
